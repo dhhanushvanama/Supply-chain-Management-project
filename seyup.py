63c833fb07ea2fa31628e4ad3a91e5ae3d3396eb
dhhanushvanama <dhhanushvanama999@gmail.com>
2025-07-28 11:25:02 +0530

    first commit

---
+++ seyup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+setup(
+    name='supply chain management',
+    author='Dhhanush',
+    author_email='dhhanushvanama999@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+    )
